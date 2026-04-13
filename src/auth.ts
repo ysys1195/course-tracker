654db12f623cfae7a599ba7b56d26a87146b1cfb
*X4H5lQQbDM <kasumi7391@gmail.com>
2026-04-13 23:22:29 +0900

    セッション取得の共通処理を追加

---
+++ src/auth.ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "database",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+  ],
+});
